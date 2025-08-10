9ec26883c5be8d15c6c62f30897f0f0a43d866dc
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-08-10 15:34:46 +0200

    Update script.sh

--- srcs/requirements/node/tools/script.sh
+++ srcs/requirements/node/tools/script.sh
@@ -3,10 +3,12 @@
 mkdir data/public
 # mkdir data/frontend
 # mkdir data/backend
+mkdir -p data/public/avatars
 
 # mv ./index.js ./data/public/index.js
 mv ./index.html ./data/public/index.html
 mv ./styles.css ./data/public/styles.css
+mv ./avatars/default-avatar.png ./data/public/avatars/default-avatar.png
 
 # mv ./index.ts ./data/frontend/index.ts
 # mv ./server.ts ./data/backend/server.ts
